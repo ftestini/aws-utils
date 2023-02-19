986b7bc53cdea668df5fbdc1f246a2d93c2846c9
Filippo Testini <filippo.testini@lynxspa.com>
2023-02-19 16:23:22 +0100

    various fixes

--- auto-stop-ec2-instances/stop-ec2-instance/lambda_function.py
+++ auto-stop-ec2-instances/stop-ec2-instance/lambda_function.py
@@ -3,28 +3,7 @@ ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     instance = event['instance_id']
-    hibernate = event['hibernate']
     print('Stopping your instances: %s' % str(instance))
-    print('Hibernate: %s' % hibernate)
-
-    # Check if hibernation input is true
-    should_hibernate = hibernate == 'True'
-    if should_hibernate:
-        # Check if instance can hibernate
-        response = ec2.describe_instance_credit_specifications(
-            InstanceIds=[instance]
-        )
-
-        credit_specifications = response['InstanceCreditSpecifications'][0]
-
-        if credit_specifications['CpuCredits']:
-            instance = ec2.stop_instances(
-                InstanceIds=[instance],
-                Hibernate=True
-            )
-            return
-
-    # If hibernation is not required or possible, then stop instead
     instance = ec2.stop_instances(
         InstanceIds=[instance]
     )
